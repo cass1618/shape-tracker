fc0a7444ecada892d92be4ead1f8e47d3ccbed7e
cass1618 <cass16180@gmail.com>
2021-04-13 11:54:52 -0700

    add and pass first test for Trianlge constructor
    Co-authored-by: Thomas Russell <t.p.russell19@gmail.com>
    Co-authored-by: Jamie Knutsen <knutsenjamie@yahoo.com>

--- src/triangle.js
+++ src/triangle.js
@@ -1,11 +1,6 @@
-export class Triangle {
-  constructor(side1, side2, side3) {
-    this.side1 = side1;
-    this.side2 = side2;
-    this.side3 = side3;
-  }
-  checkType() {
-    return "I can't answer that yet!";
-  }
+export default function Triangle(side1, side2, side3) {
+  this.side1 = side1;
+  this.side2 = side2;
+  this.side3 = side3;
 }
 
