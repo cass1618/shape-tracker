da3b41d347428319b472f1a04022fcdf1a806af7
cass1618 <cass16180@gmail.com>
2021-04-13 07:52:32 -0700

    Fixed indentation

--- src/main.js
+++ src/main.js
@@ -4,15 +4,15 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 
-  $('#triangle-checker-form').submit(function(event) {
-    event.preventDefault();
-    debugger;
-    const length1 = $('#length1').val();
-    const length2 = $('#length2').val();
-    const length3 = $('#length3').val();
-    const triangle = new Triangle(length1, length2, length3);
-    const response = triangle.checkType();
-    $('#response').append("<p>" + response + "</p>");
-  });
+$('#triangle-checker-form').submit(function(event) {
+  event.preventDefault();
+  debugger;
+  const length1 = $('#length1').val();
+  const length2 = $('#length2').val();
+  const length3 = $('#length3').val();
+  const triangle = new Triangle(length1, length2, length3);
+  const response = triangle.checkType();
+  $('#response').append("<p>" + response + "</p>");
+});
 
 
